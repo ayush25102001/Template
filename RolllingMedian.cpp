17652277bcf299a3a86e7da032a2102e15a6b4e4
Ayush Kangane <78008646+ayush25102001@users.noreply.github.com>
2024-01-24 11:11:55 +0530

    Update RolllingMedian.cpp

--- RolllingMedian.cpp
+++ RolllingMedian.cpp
@@ -3,17 +3,7 @@ class DS {
     public:
         multiset<int> s1;
         multiset<int> s2;
-        void insert(int num) {
-            if(s1.size() == 0) {
-                s1.insert(num);
-                return;
-            }
-            if(num < *(--s1.end())) {
-                s1.insert(num);
-            }
-            else {
-                s2.insert(num);
-            }
+        void balance() {
             while(s1.size() > s2.size()) {
                 s2.insert(*(--s1.end()));
                 s1.erase(*(--s1.end()));
@@ -22,6 +12,21 @@ class DS {
                 s1.insert(*(s2.begin()));
                 s2.erase(*(s2.begin()));
             }
+        }
+        void insert(int num) {
+            if(s1.size() == 0) {
+                s1.insert(num);
+                return;
+            }
+            if(num < *(--s1.end())) s1.insert(num);
+            else s2.insert(num);
+            balance();
+        }
+        void erase(int x) {
+            auto it = s2.find(x);
+            if(it != s2.end()) s2.erase(it);
+            else s1.erase(s1.find(x));
+            balance();
         }
         int getMedian() {
             return *(--s1.end());
